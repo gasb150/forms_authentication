0d0766baa7d5af82b58256cdd7b9fe7e426877f5
tavo <gasb_150@hotmail.com>
2021-01-22 13:34:51 -0500

    add edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
     def create
         #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        @user = User.new(user_params)
+        @user = User.new(params[:user_params])
 
         if @user.save
           redirect_to new_user_path
@@ -14,9 +14,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
     private
 
     def user_params
-      params.require(:user).permit(:username,:email,:password)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
