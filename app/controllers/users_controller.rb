d466356856fa6aea86724fd75064d0101363ca78
tavo <gasb_150@hotmail.com>
2021-01-22 17:03:51 -0500

    fixing save problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
+
     def new
       @user = User.new
     end
 
     def create
-        #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        @user = User.new(params[:user_params])
-
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    
+      if @user.save
+        redirect_to newasas_user_path
+      else
+        render :new
+      end
     end
+    # def create
+    #     #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #     @user = User.new(params[:user_params])
+
+    #     if @user.save
+    #       redirect_to new_user_path
+    #     else
+    #       render :new
+    #     end
+    # end
 
     def edit
       @user = User.find(params[:id])
@@ -21,7 +31,7 @@ class UsersController < ApplicationController
     def update
       @user = User.find(params[:id])
         if @user.update(params[:user_params])
-          redirect_to user_path(@user)
+          redirect_to user_path
         else
           render :edit
         end
